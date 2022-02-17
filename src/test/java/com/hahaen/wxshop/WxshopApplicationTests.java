6b42331d4e8c9129edc6d595fc185598311f1960
hahaen <hahaen.love@gmail.com>
2022-02-17 22:57:34 +0800

    chore:配置Checkstyle代码规范检查工具，并将代码规范

--- src/test/java/com/hahaen/wxshop/WxshopApplicationTests.java
+++ src/test/java/com/hahaen/wxshop/WxshopApplicationTests.java
@@ -1,13 +1,7 @@
 package com.hahaen.wxshop;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class WxshopApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
